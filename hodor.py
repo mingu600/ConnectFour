873fd700fdf109a7f97ca62a541b446395ef2c63
Mingu Kim <mingukim@college.harvard.edu>
2016-03-17 20:34:23 -0400

    fixed hodor

--- hodor.py
+++ hodor.py
@@ -4,6 +4,3 @@ def next_move(board):
         for i in range (0, connect4game.width):
             if board.state[i][j]['player'] is 0:
                 return i
-
-def height():
-    print(connect4game.height)
