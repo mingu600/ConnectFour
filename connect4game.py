e75645391599c91d187225b55f6e3403f5da1a12
Robert Gordan <rfgordan@gmail.com>
2016-03-20 22:26:10 -0400

    fixed

--- connect4game.py
+++ connect4game.py
@@ -178,7 +178,7 @@ class Game:
                 turn += 1
 
 import hodor
-import rob_bot as rg
+#import rob_bot as rg
 if __name__ == "__main__":
-    Game(rg.robbot, rg.robbot).play()
+    Game(hodor.next_move, Human).play()
 
